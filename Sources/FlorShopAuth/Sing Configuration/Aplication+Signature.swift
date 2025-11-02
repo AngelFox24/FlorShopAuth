a174fa248e487743819b0c5f7fe2ff7facf5889d
Angel Curi Laurente <curilaurente@gmail.com>
2025-11-01 20:07:31 -0500

    fix: delete CryptoKit

--- Sources/FlorShopAuth/Sing Configuration/Aplication+Signature.swift
+++ Sources/FlorShopAuth/Sing Configuration/Aplication+Signature.swift
@@ -1,16 +1,12 @@
 import Vapor
 import JWT
-import CryptoKit
 
 extension Application {
     public func setSignature() async throws {
-        guard let secret = Environment.get("JWT_SECRET"),
-              let secretDataUtf8 = secret.data(using: .utf8) else {
+        guard let secret = Environment.get("JWT_SECRET") else {
             self.logger.critical("Missing JWT_SECRET")
             throw Abort(.internalServerError, reason: "Internal Server Error")
         }
-        let key = SymmetricKey(data: secretDataUtf8)
-        let hmacKey = HMACKey(key: key)
-        await self.jwt.keys.add(hmac: hmacKey, digestAlgorithm: .sha256)
+        await self.jwt.keys.add(hmac: HMACKey(stringLiteral: secret), digestAlgorithm: .sha256)
     }
 }
